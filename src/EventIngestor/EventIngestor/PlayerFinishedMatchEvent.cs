1b8bafa1198e0d2d25d65440101c64d6d1f696be
Johannes Ebner <Structed@users.noreply.github.com>
2021-10-12 16:28:27 +0200

    Added stub function

---
+++ src/EventIngestor/EventIngestor/PlayerFinishedMatchEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace EventIngestor
+{
+    public static class PlayerFinishedMatchEvent
+    {
+        [FunctionName("PlayerFinishedMatchEvent")]
+        public static async Task RunAsync([QueueTrigger("player-finished-match-queue", Connection = "EventQueueStorage")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            
+        }
+    }
+}

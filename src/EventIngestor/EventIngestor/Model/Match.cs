a3b460f4c2d1f5e39cc448f13dc1903049c1e8b2
Johannes Ebner <Structed@users.noreply.github.com>
2021-10-12 16:28:56 +0200

    Initial Match model

---
+++ src/EventIngestor/EventIngestor/Model/Match.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EventIngestor.Model
+{
+    public class Match
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        public string PlayerEntityId { get; set; }
+
+        public bool IsMatchWon { get; set; }
+    }
+}

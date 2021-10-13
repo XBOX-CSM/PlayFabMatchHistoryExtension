d1577dbb22beb49e12e29ef337efcebcab0fd505
Andreas Pohl <andreaspohl89@gmail.com>
2021-10-13 13:46:08 +0200

    Added Public API function app for user endpoints with .NET6 and function runtime4

---
+++ PublicApi/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Util.Repository;
+
+[assembly: FunctionsStartup(typeof(PublicApi.Startup))]
+namespace PublicApi
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("CosmosDb");
+            builder.Services.AddSingleton(s => new MatchRepository(connectionString));
+        }
+    }
+}

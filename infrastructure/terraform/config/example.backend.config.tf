01598c93656497649990382a4091f59ca193d7a4
Johannes Ebner <Structed@users.noreply.github.com>
2021-10-20 14:37:48 +0200

    Cleanup example backend config

--- infrastructure/terraform/config/example.backend.config.tf
+++ infrastructure/terraform/config/example.backend.config.tf
@@ -1,4 +1,4 @@
-﻿//resource_group_name  = "StorageAccount-ResourceGroup"
-//storage_account_name = "abcd1234"
-//container_name       = "pfmatchhistory_tfstate"
-//key                  = "prod.terraform.tfstate"
+﻿#resource_group_name  = "StorageAccount-ResourceGroup"
+#storage_account_name = "abcd1234"
+#container_name       = "pfmatchhistory-tfstate"
+#key                  = "prod.terraform.tfstate"

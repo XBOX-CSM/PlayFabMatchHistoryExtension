37ad9f18789acedf026e64837f9dc16f92888f19
Johannes Ebner <Structed@users.noreply.github.com>
2021-11-16 20:27:31 +0100

    adding comments

--- src/PublicApi/GetPlayerMatchHistory.cs
+++ src/PublicApi/GetPlayerMatchHistory.cs
@@ -35,24 +35,27 @@ namespace PublicApi
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            string sessionTicket = req.Query["SessionTicket"].ToString();
+            string sessionTicket = req.Query["SessionTicket"].ToString();   // Get the SessionTicket from the Query
             
+            // PlayFab API Auth - use environment variables
             PlayFabSettings.staticSettings.TitleId = Environment.GetEnvironmentVariable("TF_VAR_pf_title_id");
             PlayFabSettings.staticSettings.DeveloperSecretKey = Environment.GetEnvironmentVariable("TF_VAR_pf_developer_secret");
 
+            // Get the TitleEntity
             var getTitleEntityTokenRequest = new GetEntityTokenRequest(); //Do not need to set Entity
             var titleEntityResponse = await PlayFabAuthenticationAPI.GetEntityTokenAsync(getTitleEntityTokenRequest);
 
             if (titleEntityResponse.Result != null)
             {
+                // Authenticate the user using the SessionTicket
                 var request = new PlayFab.ServerModels.AuthenticateSessionTicketRequest
                 {
                     SessionTicket = sessionTicket
                 };
-
                 var response = await PlayFabServerAPI.AuthenticateSessionTicketAsync(request);
                 if (response.Error != null)
                 {
+                    // Forward the error returned by PlayFab
                     var errorResponse = new ObjectResult(response.Error);
                     errorResponse.StatusCode = response.Error.HttpCode;
                     return errorResponse;
@@ -66,7 +69,7 @@ namespace PublicApi
             }
             else
             {
-                log.LogError("Couldn't validate Session Ticket with PlayFab!" + titleEntityResponse.Error);
+                log.LogError("Couldn't validate Session Ticket with PlayFab!" + titleEntityResponse.Error); // TODO: Remove - not needed
             }
 
             return new NotFoundObjectResult("No HistoryFound");
